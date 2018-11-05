06d5573a677fcc077e9e407374481a9ff55e926b
Omega <valentin.destrebecq@epitech.eu>
2018-11-05 18:33:20 +0100

    changed physics, added implementation of physics with other Unity components, added more subtle jump

---
+++ Unity_Station88/Assets/Scripts/Gameplay/PlatformController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformController : RaycastController {
+
+    public LayerMask passengerMask;
+    public Vector3 move;
+
+	public override void Start () {
+        base.Start();
+	}
+	
+	void Update () {
+        UpdateRaycastOrigins();
+
+        Vector3 velocity = move * Time.deltaTime;
+        MovePassengers(velocity);
+        transform.Translate(velocity);
+	}
+
+    void MovePassengers (Vector3 velocity)
+    {
+        HashSet<Transform> movedPassengers = new HashSet<Transform>();
+        float directionX = Mathf.Sign(velocity.x);
+        float directionY = Mathf.Sign(velocity.y);
+
+        //vertical movement
+        if (velocity.y != 0)
+        {
+            float rayLenght = Mathf.Abs(velocity.y) + skinWidth;
+            for (int i = 0; i < verticalRayCount; i++)
+            {
+                Vector2 rayOrigin = (directionY == -1)
+                    ? raycastOrigins.bottomLeft
+                    : raycastOrigins.topLeft;
+                rayOrigin += Vector2.right * (verticalRaySpacing * i);
+                RaycastHit2D hit = Physics2D.Raycast
+                    (rayOrigin, Vector2.up * directionY, rayLenght, passengerMask);
+
+                if (hit)
+                {
+                    if (!movedPassengers.Contains(hit.transform))
+                    {
+                        movedPassengers.Add(hit.transform);
+                        float pushX = (directionY == 1) ? velocity.x : 0;
+                        float pushY = velocity.y - (hit.distance - skinWidth) * directionY;
+
+                        hit.transform.Translate(new Vector3(pushX, pushY));
+                    }
+                }
+            }
+        }
+
+    }
+}
