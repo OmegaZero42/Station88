64260619c149626ffe2a287cd51263fb9edfd4ee
Omega <valentin.destrebecq@epitech.eu>
2019-01-14 18:16:29 +0100

    added inventory system basics, need ui adaptation and stats

---
+++ Unity_Station88/Assets/Scripts/Gameplay/Inventory.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour {
+
+    InventoryObjects[] objectStock;
+    InventoryObjects head;
+    InventoryObjects arms;
+    InventoryObjects accessory;
+    InventoryObjects foot;
+    InventoryObjects weapon;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+    void equipItem(InventoryObjects itemToEquip)
+    {
+        switch (itemToEquip.getType())
+        {
+            case "head":
+                head = itemToEquip;               
+                break;
+            case "arms":
+                arms = itemToEquip;
+                break;
+            case "accessory":
+                accessory = itemToEquip;
+                break;
+            case "foot":
+                foot = itemToEquip;
+                break;
+            case "weapon":
+                weapon = itemToEquip;
+                break;
+            default:
+                addItemToInv(itemToEquip);
+                break;
+        }
+    }
+
+    void addItemToInv(InventoryObjects itemToAdd)
+    {
+        int i = 0;
+        while (objectStock[i])
+        {
+            Debug.Log("going through inv");
+            i++;
+        }
+        objectStock[i + 1] = itemToAdd;
+    }
+
+}
---
+++ Unity_Station88/Assets/Scripts/Gameplay/InventoryObjects.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryObjects : MonoBehaviour {
+
+    [SerializeField]
+    string itemType;
+    [SerializeField]
+    int attackBonus;
+    [SerializeField]
+    int defenseBonus;
+
+    public int AttackBonus
+    {
+        get
+        {
+            return attackBonus;
+        }
+
+        set
+        {
+            attackBonus = value;
+        }
+    }
+
+    public int DefenseBonus
+    {
+        get
+        {
+            return defenseBonus;
+        }
+
+        set
+        {
+            defenseBonus = value;
+        }
+    }
+
+    void Start () {
+        if (!"head arms accessory foot weapon".Contains(itemType))
+        {
+            Debug.LogError("Item creation failed");
+        }
+	}
+
+
+    public string getType()
+    {
+        return(itemType);
+    }
+}
